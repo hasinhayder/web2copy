f017ac9c8987978594b77a40216b7f079d1a26c4
Hasin Hayder <hasin@leevio.com>
2014-01-15 02:53:58 +0600

    Blank URL Fix

--- web/js/main.js
+++ web/js/main.js
@@ -1,36 +1,43 @@
-;(function($){
-    $(document).ready(function(){
+;
+(function ($) {
+    $(document).ready(function () {
         $("#spinner").hide();
-        $("#web2copy, #hiw").on("click",function(){
-           $("body").scrollTo($(".features"),800);
+        $("#web2copy, #hiw").on("click", function () {
+            $("body").scrollTo($(".features"), 800);
 
         });
-        $("#tokensubmit").on("click",function(){
-            $("#spinner").show();
+        $("#tokensubmit").on("click", function () {
+            $(".urlerror").hide();
+
             var token = $("#token").val();
             var url = $("#url").val();
             var filename = $("#filename").val();
+            if (url) {
+                $("#spinner").show();
 
-            $.post("/api/token",{token:token,url:url, filename:filename},function(data){
-                if(data.error==0){
-                    $("#url").val("");
-                    $("#token").val("");
-                    $("#spinner").hide();
-                    $("#myModal").modal("hide");
+                $.post("/api/token", {token: token, url: url, filename: filename}, function (data) {
+                    if (data.error == 0) {
+                        $("#url").val("");
+                        $("#token").val("");
+                        $("#spinner").hide();
+                        $("#myModal").modal("hide");
 
-                    $("#messagetitte").html("Successful");
-                    $("#messagebody").html("Your request to transfer file was successful. We will send a notification to the token owner once the transfer is complete");
-                    $("#message").modal("show");
-                }else{
+                        $("#messagetitte").html("Successful");
+                        $("#messagebody").html("Your request to transfer file was successful. We will send a notification to the token owner once the transfer is complete");
+                        $("#message").modal("show");
+                    } else {
 
-                    $("#spinner").hide();
-                    $("#myModal").modal("hide");
+                        $("#spinner").hide();
+                        $("#myModal").modal("hide");
 
-                    $("#messagetitte").html("Error!");
-                    $("#messagebody").html('<div class="alert alert-danger">'+data.message+'</div>');
-                    $("#message").modal("show");
-                }
-            },"json")
+                        $("#messagetitte").html("Error!");
+                        $("#messagebody").html('<div class="alert alert-danger">' + data.message + '</div>');
+                        $("#message").modal("show");
+                    }
+                }, "json");
+            }else{
+                $(".urlerror").show();
+            }
         })
     })
 })(jQuery);
